efef9e21323a3cb110ca1f79ddcb5eb3d3fcc3c1
RafaelSimasS <rafaeldesousasimas@gmail.com>
2023-05-14 00:26:49 -0300

    add: created intefaces for cruds operations

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -9,3 +9,16 @@ export interface RetrieveUserFaces {
     user: string,
 
 }
+export interface DeleteUser {
+    user: string,
+}
+export interface FindUser {
+    user: string
+}
+
+export interface UpdateUser{
+    user: string,
+    images: {
+        [key: string]: string,
+    };
+}
