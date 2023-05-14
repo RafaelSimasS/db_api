2cb189ad0d68084d01ec0bc9ebf9720f409b3be5
RafaelSimasS <rafaeldesousasimas@gmail.com>
2023-05-14 00:25:54 -0300

    add: added urlencoded

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ import bodyParser from "body-parser";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', router);
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 const HOST = "192.168.1.3";
-
 
 
 app.listen(PORT, HOST, ()=>{
