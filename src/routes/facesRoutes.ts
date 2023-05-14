0e0edd3efbda1f6eb7ee8e35bd2c43a378ab4b46
RafaelSimasS <rafaeldesousasimas@gmail.com>
2023-05-14 00:28:22 -0300

    add: created api routes

--- src/routes/facesRoutes.ts
+++ src/routes/facesRoutes.ts
@@ -1,24 +1,81 @@
-import { error } from "console";
 import express from "express";
-import { create } from "../controllers/facesController";
+import {
+  createUser,
+  findUser,
+  removeUser,
+  updateUserFaces,
+} from "../controllers/facesController";
 
 const router = express.Router();
-import { SaveDataFormat } from "../interfaces/interfaces";
+import {
+  DeleteUser,
+  RetrieveUserFaces,
+  SaveDataFormat,
+  UpdateUser,
+} from "../interfaces/interfaces";
 
-router.get("/date", (req, res) => {
-  console.log("Hello World");
+router.get("/test", async (req, res) => {
+  console.log("Testing");
+  const data = req.body as SaveDataFormat;
+  const { user } = data;
+  const userName = { user };
+  const result = await findUser(userName);
+  console.log(result);
   res.status(200).send(new Date());
 });
 
-router.post("/save-faces", (req, res) => {
-  try {
-    const data = req.body as SaveDataFormat;
-    console.log(data);
-    create(data);
-    res.status(200).send("Saved data to db!");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error saving data!");
+router.post("/create-user", async (req, res) => {
+  const data = req.body as SaveDataFormat;
+  const { user } = data;
+  const userName = { user };
+  const userExist = await findUser(userName);
+  if (!userExist) {
+    try {
+      await createUser(data);
+      res.status(200).send("User created!");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("[Error] - Not able to create user!");
+    }
+  } else {
+    res.status(409).send("[Error] - User already exists!");
+  }
+});
+
+router.delete("/delete-user", async (req, res) => {
+  const data = req.body as DeleteUser;
+  const { user } = data;
+  const userName = { user };
+  const userExist = await findUser(userName);
+  if (userExist) {
+    try {
+      const result = await removeUser(data);
+      console.log(result);
+      res.status(200).send("Deleted user!");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error deleting user.");
+    }
+  } else {
+    res.status(404).send("User does not exists!");
+  }
+});
+router.put("/update-user", async (req, res) => {
+  const data = req.body as UpdateUser;
+  const { user } = data;
+  const userName = { user };
+  const userExist = await findUser(userName);
+  if (userExist) {
+    try {
+      const result = await updateUserFaces(data);
+      console.log(result);
+      res.status(200).send("Updated user!");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error Updating user.");
+    }
+  } else {
+    res.status(404).send("User does not exists!");
   }
 });
 
