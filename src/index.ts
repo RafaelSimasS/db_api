a92644ce28bab4184943d8aa3cbf32613e0e0b61
RafaelSimasS <rafaeldesousasimas@gmail.com>
2023-09-22 13:02:34 -0300

    feat: removed HOST IP from express

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,9 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use("/", router);
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-const HOST = "192.168.1.10"; //192.168.42.30
+const HOST = process.env.HOST || "192.168.1.14";
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://${HOST}:${PORT}/`);
 });
+module.exports = app;
